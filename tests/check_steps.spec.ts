9a28143909926d87d3a6bd41c91c8b2e293d20cb
curtis taylor <curtis.t@gmail.com>
2025-04-25 02:54:26 -0400

    2nd commit for Playwright FE tests. Worked on Check Steps page

--- tests/check_steps.spec.ts
+++ tests/check_steps.spec.ts
@@ -1,16 +1,38 @@
 import { test, expect } from '@playwright/test';
 import { execPath } from 'process';
+test.describe('Account Confirmed test', () => {
+  test('HEADER text', async ({ page }) => {
+    await page.goto('http://localhost:3000/pages/check-steps/');
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/pages/profiles/');
+    // await expect(page.locator('li').nth(1)).toHaveText('Account confirmed');
 
+    await page.getByRole('listitem', {name: 'Account confirmed'}).isVisible();
+    await page.getByRole('listitem', {name: 'Check the conduct code'}).isVisible();
+    await page.getByRole('listitem', {name: 'Complete your profile'}).isVisible();
 
+    // Expect a title "to contain" a substring.
+    // await expect(page.locator('h3')).toHaveText('Volunteer Profile');
 
-  // Expect a title "to contain" a substring.
-  // await expect(page.locator('h3')).toHaveText('Volunteer Profile');
+    for (const row of await page.locator('h3').all()) {
+      console.log(await row.textContent());
+      expect(await row.textContent() == 'Volunteer Profile');
+    }
+  });
 
-  for (const row of await page.locator('h3').all()) {
-    console.log(await row.textContent());
-    expect(await row.textContent() == 'Volunteer Profile');
-  }
+  test('PAGE elements', async ({ page }) => {
+    await page.goto('http://localhost:3000/pages/check-steps/');
+
+    // await expect(page.locator('li').nth(1)).toHaveText('Account confirmed');
+
+    await page.getByRole('heading', {name: 'Welcome to TorontoJS Community Hub!'}).isVisible();
+
+    await page.getByRole('link', {name: "Let's continue"}).isVisible();
+    await page.getByRole('link', {name: "Let's continue"}).click();
+
+    // Expect a title "to contain" a substring.
+    // await expect(page.locator('h3')).toHaveText('Volunteer Profile');
+
+   
+  });
+
 });
