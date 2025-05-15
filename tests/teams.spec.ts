5c068e0bea9ce9c8410eaef7f8633a3434bc3683
curtis taylor <curtis.t@gmail.com>
2025-05-15 03:15:10 -0400

    Seemed to get Teams link issue to work

--- tests/teams.spec.ts
+++ tests/teams.spec.ts
@@ -34,20 +34,115 @@ test('Design Dragon links', async ({ browser }) => {
      
      let [newPage_1] = await Promise.all([
             browser_context.waitForEvent("page"), // pending, fullfilled or rejected
-            await row.click()     
+            await row.click()    
         ]);
 
      // expect(newPage_1.getByRole('link', { name: 'About' } ));
         
-      newPage_1.waitForTimeout(5000)
-      let pp = await newPage_1.evaluate(() => window.location.href);
-      console.log(pp);
-      // console.log("----");
+      // newPage_1.waitForTimeout(1000);
+      console.log(newPage_1.url());
+      let pp = newPage_1.url();
+      // let pp = await newPage_1.evaluate(() => window.location.href);
+      // console.log(pp);
 
       expect(pp.includes("linkedin.com"));
 
       await newPage_1.close();
     // expect(await row.textContent() == 'Volunteer Profile');
+
+    row.hover();
+
+  } 
+
+
+
+  await page.close();
+});
+
+test('DEVELOPMENT DRUIDS links', async ({ browser }) => {
+
+    const browser_context = await browser.newContext();
+    const page = await browser_context.newPage();
+
+    await page.goto("http://localhost:3000/pages/team/"); 
+
+  await page.waitForTimeout(2000);
+
+  for (const row of await page.getByText('Toronto JS\' tournament wizards!Team membersJohn DoelinkedinJane').getByRole("link").all()) {
+     let temp = await row.textContent();
+     console.log(temp);
+     
+     let [newPage_1] = await Promise.all([
+            browser_context.waitForEvent("page"), // pending, fullfilled or rejected
+            await row.click()    
+        ]);
+
+      console.log(newPage_1.url());
+      let pp = newPage_1.url();
+
+      expect(pp.includes("linkedin.com"));
+
+      await newPage_1.close();
+  } 
+
+  await page.close();
+});
+
+test('COMMUNITY CRAFTERS links', async ({ browser }) => {
+
+    const browser_context = await browser.newContext();
+    const page = await browser_context.newPage();
+
+    await page.goto("http://localhost:3000/pages/team/"); 
+
+  await page.waitForTimeout(2000);
+
+  for (const row of await page.getByText('Toronto JS\' tournament wizards!Team membersJohn DoelinkedinJane').getByRole("link").all()) {
+     let temp = await row.textContent();
+     console.log(temp);
+     
+     let [newPage_1] = await Promise.all([
+            browser_context.waitForEvent("page"), // pending, fullfilled or rejected
+            await row.click()    
+        ]);
+
+      console.log(newPage_1.url());
+      let pp = newPage_1.url();
+
+      expect(pp.includes("linkedin.com"));
+
+      await newPage_1.close();
+  } 
+
+
+
+  await page.close();
+});
+
+test('OMNIPOTENT ORGANISERS links', async ({ browser }) => {
+
+    const browser_context = await browser.newContext();
+    const page = await browser_context.newPage();
+
+    await page.goto("http://localhost:3000/pages/team/"); 
+
+  await page.waitForTimeout(2000);
+
+  for (const row of await page.getByText('Toronto JS\' overseeing overlords!Team membersJohn DoelinkedinJane').getByRole("link").all()) {
+     let temp = await row.textContent();
+     console.log(temp);
+     
+     let [newPage_1] = await Promise.all([
+            browser_context.waitForEvent("page"), // pending, fullfilled or rejected
+            await row.click()    
+        ]);
+
+      console.log(newPage_1.url());
+      let pp = newPage_1.url();
+
+      expect(pp.includes("linkedin.com"));
+
+      await newPage_1.close();
   } 
 
   await page.close();
