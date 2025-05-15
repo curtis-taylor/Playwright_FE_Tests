b02f84220ade57876aedd5b97949c4001d30af2b
curtis-taylor <curtis.t@gmail.com>
2025-05-14 23:05:26 -0400

    Continue work on Teams test

--- tests/teams.spec.ts
+++ tests/teams.spec.ts
@@ -8,37 +8,47 @@ test('Design Dragon links', async ({ browser }) => {
 
     await page.goto("http://localhost:3000/pages/team/"); 
 
-    const design_team = await page.getByText('Toronto JS').getByRole("link").all();
+   // const design_team = await page.getByText('Toronto JS').getByRole("link").all();
 
-    for(const cc of await page.getByText('Toronto JS\' awesome creative minds!Team members').getByRole("link").all()) {
-        console.log(await cc.textContent());
-        console.log("::");
+   /*
+   console.log("****");
+   let i = 1;
+   await page.waitForTimeout(2000);
+    for(const cc2 of await page.locator(".team-member-card .team-member-profile-link").getByRole("link").all()) {
+        console.log(await cc2.textContent());
+        console.log(i);
+        i++;
         await page.waitForTimeout(2000);
     }
-
-  for (const row of await page.locator('.social-links a').all()) {
+    */
+  
+  await page.waitForTimeout(2000);
+  console.log((await page.getByText('Toronto JS\' awesome creative minds!Team members').getByRole("link").all()).length);
+  
+  for (const row of await page.getByText('Toronto JS\' awesome creative minds!Team members').getByRole("link").all()) {
      let temp = await row.textContent();
      console.log(temp);
+     console.log(await page.evaluate(() => window.location.href));
+
+     
      
      let [newPage_1] = await Promise.all([
             browser_context.waitForEvent("page"), // pending, fullfilled or rejected
-            await row.click()
+            await row.click()     
         ]);
+
+     // expect(newPage_1.getByRole('link', { name: 'About' } ));
         
+      newPage_1.waitForTimeout(5000)
       let pp = await newPage_1.evaluate(() => window.location.href);
       console.log(pp);
       // console.log("----");
 
-      if(temp != "Twitter") { 
-        expect(pp).toContain(temp?.toLowerCase());
-
-      } else {
-         expect(pp).toContain("x.com");
-      }
+      expect(pp.includes("linkedin.com"));
 
       await newPage_1.close();
     // expect(await row.textContent() == 'Volunteer Profile');
-  }
+  } 
 
   await page.close();
 });
