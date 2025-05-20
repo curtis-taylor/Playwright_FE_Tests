390814d31a1e0685639cb780c9774b4183d799ae
curtis taylor <curtis.t@gmail.com>
2025-05-20 03:09:12 -0400

    Started work on POM for Complete Profile

---
+++ page_object_models/pom_complete_profile.ts
@@ -0,0 +1,80 @@
+import { test, expect, Page, Locator } from '@playwright/test';
+
+class CompleteProfilePage {
+    readonly page: Page;
+    readonly url: String = 'https://26-profile-page-css.volunteer-ekr.pages.dev/pages/complete-profile/';
+    readonly name_field: Locator;
+    readonly email_field: Locator;
+    readonly slack_field: Locator;
+    readonly pronouns: Locator;
+    readonly dob_Month: Locator;
+    readonly dob_Day: Locator;
+
+    readonly based_GTA_switch: Locator;
+    readonly can_join_Local_switch: Locator;
+    readonly upload_Button: Locator;
+    readonly file_picker: Locator;
+
+    readonly linkedin_profile_1: Locator;
+    readonly github_field: Locator;
+    readonly site_field: Locator;
+    readonly skills_field: Locator;
+
+    readonly instagram_field: Locator;
+    readonly linkedin_2nd_field: Locator;
+
+    readonly facebook_icon: Locator;
+    readonly instagram_icon: Locator;
+    readonly threads_icon: Locator;
+    readonly bluesky_icon: Locator;
+    readonly twitter_x_icon: Locator;
+    readonly linkedin_icon: Locator;
+    readonly dev_icon: Locator;
+
+    readonly complete_button: Locator;
+    readonly nutshell_bar: Locator;
+    readonly avatar_bar: Locator;
+    readonly more_info_bar: Locator;
+
+    public constructor(page: Page) {
+        this.page = page;
+        this.name_field = page.getByRole('textbox', { name: 'Name' });
+        this.email_field = page.getByRole('textbox', { name: 'E-mail REQUIRED' });
+        this.slack_field = page.getByRole('textbox', { name: 'Slack handle Required" / "' });
+        this.pronouns = page.getByRole('combobox', { name: 'Pronouns' });
+        this.dob_Month = page.getByLabel('Month');
+        this.dob_Day = page.getByLabel('Day');
+
+        this.based_GTA_switch = page.getByText('I\'m based in Toronto or');
+        this.can_join_Local_switch = page.getByText('I can join TorontoJS\'s local');
+        this.upload_Button = page.getByRole('button', { name: 'Upload Your Photo' });
+        this.file_picker = page.locator("#image-upload");
+        this.linkedin_profile_1 = page.getByRole('textbox', { name: 'LinkedIn profile' });
+        this.github_field = page.getByRole('textbox', { name: 'GitHub profile' });
+        this.site_field = page.getByRole('textbox', { name: 'Site/portfolio' });
+        this.skills_field = page.getByRole('textbox', { name: 'Your skills' });
+
+        this.nutshell_bar = page.locator('summary').filter({ hasText: 'In a nutshell:' });
+        this.avatar_bar = page.locator('summary').filter({ hasText: 'Avatar:' });
+        this.more_info_bar = page.locator('summary').filter({ hasText: 'More Information:' });
+
+        this.instagram_field = page.getByRole('textbox', {name: 'Instagram'});
+        this.linkedin_2nd_field = page.getByRole('textbox', {name: 'LinkedIn'}).nth(1);
+
+    }
+    
+
+        const facebook_icon = page.getByRole('button', { name: 'Add Facebook account' });
+        const threads_icon = page.getByRole('button', { name: 'Add Threads account' });
+        const instagram_icon = page.getByRole('button', { name: 'Add Instagram account' });
+        const twitter_x_icon = page.getByRole('button', { name: 'Add X account' });
+        const linkedin_icon = page.getByRole('button', { name: 'Add LinkedIn account' });
+        const bluesky_icon = page.getByRole('button', { name: 'Add BlueSky account' })
+        const dev_icon = page.getByRole('button', { name: 'Add Dev.to account' });
+
+        const complete_button = page.getByRole('button', {name: 'Complete My Profile'});
+
+        const button_list = [facebook_icon, threads_icon, instagram_icon, twitter_x_icon, linkedin_icon, bluesky_icon, dev_icon];
+
+        
+}
